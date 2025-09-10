6c6dc803f7859496c0148b1419d51de3d7f28369
Hamza Alsakkal <130228445+HamzaAlsakkal@users.noreply.github.com>
2025-09-11 01:40:06 +0300

    Initial commit: E-commerce NestJS API with User Management

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,24 @@
+import {
+  IsString,
+  IsEmail,
+  IsEnum,
+  IsOptional,
+  MinLength,
+} from 'class-validator';
+import { UserRole } from '../entites/user.entity';
+
+export class CreateUserDto {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsEnum(UserRole)
+  @IsOptional()
+  role?: UserRole;
+}
